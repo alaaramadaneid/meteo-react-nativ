a3a1db3c18e79222e6b2024dc1c7e71b82017fed
admin <admin@iMac-ITK-02.local>
2024-01-30 16:06:41 +0100

    ajouter icon

--- meteo_app/App.js
+++ meteo_app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Platform ,FlatList} from 'react-native';
+import { StyleSheet, Text, View, Platform ,FlatList,Image} from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
@@ -8,12 +8,13 @@ export default function App() {
   const [errorMsg, setErrorMsg] = useState(null);
   const [isLoding,setLoding]= useState(true);
   const [data,setData]=useState([]);
-
   const getMeteo =async ()=>{
     try{
-      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=45.78190100248175&lon=4.748340591070032&appid=95435d65d392e73961b46d9001d1df26');
+      
+      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=45.78190100248175&lon=4.748340591070032&appid=95435d65d392e73961b46d9001d1df26&units=metric');
       const json =await response.json();
       setData(json);
+   console.log(json);
     }
     catch(error){
       console.error(error);
@@ -46,19 +47,26 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{data.name}</Text>
-      <Text style={styles.paragraph}>{data.weather[0].description}</Text>
-      <Text style={styles.paragraph}>{data.main.temp}</Text>
-    </View>
+    <Text style={styles.paragraph}>{data.name}</Text>
+    {data.weather && data.weather[0] && (
+      <Image style={styles.image} source={{ uri: `http://openweathermap.org/img/w/${data.weather[0].icon}.png` }} />
+    )}
+       <Text style={styles.paragraph}>{data.main && data.main.temp}</Text>
+
+  </View>
+  
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#BAE8EA',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width:100,
+    height:100,
+  }
 });

2079fc8478fecd1ea1c19b5e56fe73930a5948da
alaa <aedalaa36@gmail.com>
2024-01-30 19:17:17 +0100

    correction

--- meteo_app/App.js
+++ meteo_app/App.js
@@ -1,60 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Platform ,FlatList,Image} from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [isLoding,setLoding]= useState(true);
-  const [data,setData]=useState([]);
-  const getMeteo =async ()=>{
-    try{
-      
-      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=45.78190100248175&lon=4.748340591070032&appid=95435d65d392e73961b46d9001d1df26&units=metric');
-      const json =await response.json();
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+
+  const getWeatherData = async (latitude, longitude) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=95435d65d392e73961b46d9001d1df26&units=metric`
+      );
+      const json = await response.json();
       setData(json);
-   console.log(json);
-    }
-    catch(error){
+      console.log(json);
+    } catch (error) {
       console.error(error);
-    }
-    finally{
-      setLoding(false);
+    } finally {
+      setLoading(false);
     }
   };
-  useEffect(()=>{
-    getMeteo();
-  },[]);
-  
+
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location is required!');
-        return;
+    const getLocation = async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location is required!');
+          return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+
+        if (location.coords.latitude && location.coords.longitude) {
+          await getWeatherData(location.coords.latitude, location.coords.longitude);
+        }
+      } catch (error) {
+        console.error(error);
       }
-      let location = await Location.getCurrentPositionAsync({})
-      setLocation(location);
-    })();
+    };
+
+    getLocation();
   }, []);
-  let text= 'Waiting..'
+
+  let text = 'Waiting...';
+
   if (errorMsg) {
-    text= errorMsg
-  }else{
-    text = JSON.stringify(location)
+    text = errorMsg;
+  } else if (location) {
+    text = `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`;
   }
 
   return (
     <View style={styles.container}>
-    <Text style={styles.paragraph}>{data.name}</Text>
-    {data.weather && data.weather[0] && (
-      <Image style={styles.image} source={{ uri: `http://openweathermap.org/img/w/${data.weather[0].icon}.png` }} />
-    )}
-       <Text style={styles.paragraph}>{data.main && data.main.temp}</Text>
-
-  </View>
-  
+      <Text style={styles.paragraph}>{data.name}</Text>
+      {data.weather && data.weather[0] && (
+        <Image style={styles.image} source={{ uri: `http://openweathermap.org/img/w/${data.weather[0].icon}.png` }} />
+      )}
+      <Text style={styles.paragraph}>{data.main && data.main.temp}</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
@@ -65,8 +74,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image:{
-    width:100,
-    height:100,
-  }
+  paragraph: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
 });

6c9a8dc759bdcae13e3910771d5621825c659810
admin <admin@iMac-ITK-02.local>
2024-01-30 14:01:55 +0100

    mis à jour

--- meteo_app/App.js
+++ meteo_app/App.js
@@ -1,14 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Platform ,FlatList} from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [isLoding,setLoding]= useState(true);
+  const [data,setData]=useState([]);
+
+  const getMeteo =async ()=>{
+    try{
+      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=45.78190100248175&lon=4.748340591070032&appid=95435d65d392e73961b46d9001d1df26');
+      const json =await response.json();
+      setData(json);
+    }
+    catch(error){
+      console.error(error);
+    }
+    finally{
+      setLoding(false);
+    }
+  };
+  useEffect(()=>{
+    getMeteo();
+  },[]);
+  
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location is required!');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location);
+    })();
+  }, []);
+  let text= 'Waiting..'
+  if (errorMsg) {
+    text= errorMsg
+  }else{
+    text = JSON.stringify(location)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{data.name}</Text>
+      <Text style={styles.paragraph}>{data.weather[0].description}</Text>
+      <Text style={styles.paragraph}>{data.main.temp}</Text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
